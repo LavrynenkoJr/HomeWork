b5726d7c05832ed686e59c13bd54f80fd9da556d
Cyborg <s.o.lavrynenko@gmail.com>
2017-01-31 21:35:14 +0200

    1.  Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия» - «Имя». Проверить сколько людей имеют совпадающие с заданным имя или фамилию. 2.  Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: «фамилия» - «дата рождения». Удалить из словаря всех людей, родившихся летом.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,26 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        1.  Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия» - «Имя».
+        Проверить сколько людей имеют совпадающие с заданным имя или фамилию.
+        2.  Создать словарь (Map<String, Date>) и занести в него десять записей по принципу: «фамилия» - «дата рождения».
+        Удалить из словаря всех людей, родившихся летом.
+         */
+        countEqualsByStringFromMap("Алексей");
+        removeAllWhoBbornInSummer();
+
+        /*
+        Дана действительная квадратная матрица порядка N (N – нечетное), все элементы которой различны.
+        Найти наибольший элемент среди стоящих на главной и побочной диагоналях и поменять его местами с элементом,
+        стоящим на пересечении этих диагоналей.
+         */
         maxtToCenterFromArrayDiagonal(createArray(5));
 
         //24 задача
@@ -14,6 +29,60 @@ public class Main {
        // printVampir();
 
     }
+
+    private static void removeAllWhoBbornInSummer() {
+
+        Map<String, Date> mapBorn = new HashMap<>();
+        mapBorn.put("Viktor1", new Date(1990,0,10));
+        mapBorn.put("Viktor2", new Date(1990,2,10));
+        mapBorn.put("Viktor3", new Date(1990,3,10));
+        mapBorn.put("Viktor4", new Date(1990,4,10));
+        mapBorn.put("Viktor5", new Date(1990,6,10));//leto month 7
+        mapBorn.put("Viktor6", new Date(1990,6,10));//leto month 7
+        mapBorn.put("Viktor7", new Date(1990,7,10));//leto month 8
+        mapBorn.put("Viktor8", new Date(1990,8,10));
+        mapBorn.put("Viktor9", new Date(1990,9,10));
+        mapBorn.put("Viktor10", new Date(1990,11,10));
+
+        System.out.println(mapBorn);
+
+        for(Iterator<Map.Entry<String, Date>> entryIterator = mapBorn.entrySet().iterator(); entryIterator.hasNext(); ) {
+            Map.Entry<String, Date> entry = entryIterator.next();
+            if(entry.getValue().getMonth()>4 && entry.getValue().getMonth()<8) {
+                entryIterator.remove();
+                System.out.println("Удаляем = " + entry.getKey());
+            }
+        }
+        System.out.println(mapBorn);
+    }
+
+    public static void countEqualsByStringFromMap(String name){
+        int countNames = 0;
+        int countSurname = 0;
+
+        Map<String, String> map = new HashMap<>();
+        map.put("Фамилия1","Андрей");
+        map.put("Фамилия2","Анатолий");
+        map.put("Фамилия3","Владимир");
+        map.put("Фамилия4","Павел");
+        map.put("Фамилия5","Сергей");
+        map.put("Фамилия6","Алексей");
+        map.put("Фамилия7","Алексей");
+        map.put("Фамилия8","Алексей");
+        map.put("Фамилия9","Алексей");
+        map.put("Фамилия10","Алексей");
+
+        for (Map.Entry<String, String> names: map.entrySet()) {
+            if (names.getValue().equals(name)){
+                countNames++;
+            }else if (names.getKey().equals(name)){
+                countSurname++;
+            }
+        }
+        System.out.println("Совпадающих имен = " + countNames);
+        System.out.println("Совпадающих фамилий = " + countSurname);
+    }
+
 
     public static int[][] createArray(int n){
         int[][] array = new int[n][n];
@@ -26,7 +95,6 @@ public class Main {
         }
 
         Iterator<Integer> iterator = set.iterator();
-
         while (iterator.hasNext()) {
            for (int i = 0; i < n; i++) {
                for (int k = 0; k < n; k++) {
@@ -34,7 +102,6 @@ public class Main {
                }
            }
         }
-
         for (int i = 0; i < n; i++) {
             for (int p = 0; p < n; p++) {
                 System.out.print(array[i][p] + "\t");
